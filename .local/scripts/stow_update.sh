53ff975bf3795e2654f49735bf5031eca993a87a
thinkpadL470 <happywaveletuser@mailbox.org>
2025-01-06 10:45:46 +0100

    horrible change

--- .local/scripts/stow_update.sh
+++ .local/scripts/stow_update.sh
@@ -1,7 +1,17 @@
 #!/bin/dash
-for folder in /.dotfiles /.dotfiles_privet
+[ -d "${HOME}/.dotfiles" ] &&
+    stow_foos="${HOME}/.dotfiles"
+[ -d "${HOME}/.dotfiles_privet" ] &&
+    stow_foos="${stow_foos} ${HOME}/.dotfiles_privet"
+for folder in ${stow_foos}
 do
-    cd ${HOME}${folder}
-    stow -R .
+    cd ${folder}
+    stow -R --no-folding .
+    { cat ${HOME}/.config/find/dotfiles* |
+        while IFS= read -r target
+        do
+            rm -r ${HOME}/${target} 2>/dev/null
+        done ;
+    }
     git assch
 done
