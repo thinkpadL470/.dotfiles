8ce5f16666872cd0f83cb72e7849c55e30c9f88a
thinkpadL470 <happywaveletuser@mailbox.org>
2025-08-11 12:51:32 +0200

    horrible change

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -18,6 +18,11 @@
     
     # The home.packages option allows you to install Nix packages into your
     # environment.
+    nixpkgs = {
+        config = {
+            allowUnfree = true;
+        };
+    };
     home.packages = [
         pkgs.librewolf-wayland
         pkgs.rip2
